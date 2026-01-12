fce049b34ccf551ede867e5aa66db949b492f36e
Frane Krapić <krapicf@gmail.com>
2026-01-12 23:39:14 +0100

    feat: Export new GUI widgets from package

--- csv_visualizer/gui/__init__.py
+++ csv_visualizer/gui/__init__.py
@@ -1,5 +1,29 @@
 """GUI modul."""
 
 from .app import Application
+from .widgets import (
+    Tooltip,
+    ModernButton,
+    ThemeToggle,
+    StatsPanel,
+    DataTable,
+    FilterPanel,
+    SearchBar,
+    StatusBar,
+    LoadingIndicator,
+    EmptyState,
+)
 
-__all__ = ["Application"]
+__all__ = [
+    "Application",
+    "Tooltip",
+    "ModernButton",
+    "ThemeToggle",
+    "StatsPanel",
+    "DataTable",
+    "FilterPanel",
+    "SearchBar",
+    "StatusBar",
+    "LoadingIndicator",
+    "EmptyState",
+]
